0036a2fe007b4f59f3de40221ddfadcc4b887ef5
Pavel <krobynetspavel@gmail.com>
2025-01-20 19:27:34 +0200

    started services section

---
+++ src/app/components/services/Services.tsx
@@ -0,0 +1,16 @@
+import Title from "../title/Title";
+export default function Services() {
+  return (
+    <section>
+      <div>
+        <Title title="what we do" />
+        <h3>Services We Provide</h3>
+        <p>
+          Mestib ulligula remove the politices project completed marketing
+          reserch abusiness high level process consultation for business.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+}
